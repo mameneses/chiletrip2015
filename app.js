407646ca168546db29b9c2b71b1f1d48e3aa24a0
Matias Meneses <matiasameneses@gmail.com>
2015-01-06 23:37:42 -0800

    added more funcitonality

--- app.js
+++ app.js
@@ -1,17 +1,42 @@
-(function(){
-  var app = angular.module('trip', [ ]);
-
-  app.controller('TripController', function TripController($scope){
-    var store = this;
-    store = [ ];
-    var locations =["SFO", "Pucon", "Villarrica", "Santiago", "VinaDelMar"]
-    var instagramApi = function(location) {
-      return 'https://api.instagram.com/v1/tags/' + location + '/media/recent?access_token=ACCESS-TOKEN'
-    };
-    for (var i=0, i < locations.length, i++){
-      $http.get(instagramApi(locations[i]);).success(function(data){
-
-      });
-    };
+$( document ).ready(function() {
+  $(".pucon").on("click", function() {     
+      $('.puconinfo').slideToggle("slow");        
+  }); 
+  $(".arrive").on("click", function() {     
+      $('.arriveinfo').slideToggle("slow");        
   });
-})();
+  $(".santiago").on("click", function() {     
+      $('.santiagoinfo').slideToggle("slow");        
+  });
+
+  $(".pineda").on("click", function() {     
+      $('.pinedainfo').slideToggle("slow");        
+  });
+
+  $(".wedding").on("click", function() {     
+      $('.weddinginfo').slideToggle("slow");        
+  });
+
+  $(".vina").on("click", function() {     
+      $('.vinainfo').slideToggle("slow");        
+  });
+
+  $(".depart").on("click", function() {     
+      $('.departinfo').slideToggle("slow");        
+  });
+
+  $(".packing").on("click", function() {     
+      $('.packinginfo').slideToggle("slow");        
+  });
+});
+
+// $( document ).ready(function() {
+//   var tileClass = ["pucon","arrive","santiago","pineda","wedding","vina","depart"];
+//   for ( var i = 0; i < tileClass.length; i++) {
+//     var tile = "." + tileClass[i];
+//     var tileInfo = "." + tileClass[i] + "info";
+//     $(tile).on("click", function() {     
+//         $(tileInfo).slideToggle("slow");        
+//     }); 
+//   };
+// });
